10033e5432bd176fdb231638b7037b15382b7e5d
Amit Portnoy <amit.portnoy@gmail.com>
2015-09-12 14:28:06 +0300

    make clinics collapsible

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ import angular from 'angular';
 import 'angular-material';
 import './stylesheets/index.css!';
 
-angular.module('isradoc', ['ngMaterial']);
+const app = angular.module('isradoc', ['ngMaterial']);
+
+app.controller('clinicsController', ['$scope', function ($scope) {
+    $scope.clincs = [{
+        show: true
+    }]
+}]);
 
 angular.element(document).ready(function () {
     angular.bootstrap(document, ['isradoc'], {strictDi: true});
